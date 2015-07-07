adb8e3ceea4f460e2ef1cd9ee84af0441a20cd8a
Reed Trevelyan <reedtrevelyan@gmail.com>
2015-07-07 17:00:26 -0400

    There's some weird competing files going on right now. These are the versions i'm using

--- LoginScreen.php
+++ LoginScreen.php
@@ -1,12 +1,10 @@
 <?php
-
   session_start();
   
   require_once("dbLogin.php");
   require_once("sqlconnector.php");
   
   if (isset($_POST["Login"])) {
-	 //Change user,password,database, and table with correct ones//
 	 $host = "localhost";
 	 $dbuser = "user";
 	 $dbpassword = "user";
@@ -33,37 +31,7 @@
 		}
 	 }
 	 
-	/* if ($result) {
-		$numberOfRows = mysqli_num_rows($result);
-		  if ($numberOfRows == 0) {
-				echo "Whoops! Seems like you haven't signed up yet! Click below to sign up!";
-		  } else {
-	         while ($recordArray = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-		     	$User = $recordArray['username'];
-				    if ($User === $username) {
-						$_SESSION['username'] = trim($_POST['username']);
-						//header('Location: main.php');
-					} else {
-					   echo "Whoops! Seems like you haven't signed up yet! Click below to sign up!".mysqli_error($db);
-					}
-				}
-		  }
-	 } */
-  } elseif (isset($_POST["signup"])) {
-	 echo "Sign up";
-	 header("Location: signup.php");
   }
-  
-  /**********************************************************************/
-  /*function connectToDB($host, $user, $password, $database) {
-    $db = mysqli_connect($host, $user, $password, $database);
-	  if (mysqli_connect_errno()) {
-		  echo "Connect failed.\n".mysqli_connect_error();
-		  exit();
-	  }
-	  return $db;
-	} */
-  /**********************************************************************/
 ?>
 
 <html>
@@ -86,7 +54,6 @@
         $scriptName = $_SERVER["PHP_SELF"];
         $username = "";
         $password = "";
-
         echo "<form action='$scriptName' method='post'>
 			<p>
                 <input type='text' placeholder='Username' required='required' name='username' value ='$username'/>
@@ -102,9 +69,12 @@
                 </br>
                 </br>
 				<p>New to CalendApp? Sign Up today!</p>
-				<input id= 'sign' type='submit' name='signup' value='Sign Up' />           
+				</form>
+				<form action='signup2.php' method='post'>
+				<input id= 'sign' type='submit' name='signup' value='Sign Up' />
+				</form>
 			</p></br>
-		</form>";
+		";
         ?>
         </div>
    </body>
