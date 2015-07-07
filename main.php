d01f2b8b9908825c0b187c42dea8ddf405b99a43
Reed Trevelyan <reedtrevelyan@gmail.com>
2015-07-07 17:42:43 -0400

    This is the correct version of main

--- main.php
+++ main.php
@@ -1,17 +1,13 @@
 <?php
     
-    //This needs to be added to main.php//
+    include_once("support.php");
+    include_once("dbLogin.php");
+    include_once("sqlconnector.php");
+    
+    session_start();
     echo "<link rel='stylesheet' href='main.css' type='text/css' />";
     echo "<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>";
     echo "<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>";
-    ////////////////////////////////////////////////////////////////////////////
-    //I commented some of these lines out because I couldn't see the page//
-   /////////////////////////////////////////////////////////////////////////////
-   
-    include_once("support.php");
-    //include_once("dbLogin.php");
-    //include_once("sqlconnector.php");
-
     if(isset($_POST['submitButton'])){
         
         $month = $_POST['month'];
@@ -24,57 +20,65 @@
         
     }
     
-    //$cred = new Credentials("localhost", "user", "user", "calendapp"); //update these to real values once we make the db
-    //$connection = new SQLConnector($cred);
-    //$connection->connect();
+    $cred = new Credentials("localhost", "user", "user", "calendapp"); //update these to real values once we make the db
+    $connection = new SQLConnector($cred);
+    $connection->connect();
     
-    //$user = $_COOKIE['username'];
-    $user = "test";
+    if (isset($_SESSION['username'])) {
+        $user = $_SESSION['username'];
+    } else {
+        $user = "test";
+    }
+    
     $query = "select profilepic from users where name=\"$user\""; //this kind of assumes user is the primary key, could rework with email
-    $profpic = 'default.jpg';
+    $profpic = $connection->retrieve($query);
     
     //insert logo here, float left the month/year? float right the profile info so it's in the upper right corner
     //could make icon graphics for edit and upload
     //should probably include a logout button somewhere
+    
     $body =<<<HEREDOC
         
         <a href="main.php"> <img src="img/CalendAppLogo.png" width="256" height="73" alt="CalendApp" id="logo"> </a>
         <span id="calendarTitle">
             <h1>$month $year</h1>
-            <h2>University of Maryland, College Park</h2>
         </span>
         <div id="profile">
-            
-            
-            <img src=$profpic alt=$user width='50' height='50'>
-           
-           
+            <img src=$profpic[profilepic] alt=$user width='50' height='50'>
             Hello, $user!
             <br />
             <a href='profile.php'>Edit Profile</a> 
             <a href='upload.php'>Upload Photo</a>
+            <a href='logout.php'>Logout</a>
         </div>
         <span id="sidebar">
             <form action="main.php" method="post" id="side">
                 <select name="month">
-                    <option value="January">January
-                    <option value="February">February
-                    <option value="March">March
-                    <option value="April">April
-                    <option value="May">May
-                    <option value="June">June
-                    <option value="July">July
-                    <option value="August">August
-                    <option value="September">September
-                    <option value="October">October
-                    <option value="November">November
-                    <option value="December">December
-                </select>
-                <select name="year">
 HEREDOC;
+    $months = array("January", "February", "March", "April", "May", "June", "July",
+                "August", "September", "October", "November", "December");
+    
+    //Keeps current month selected
+    foreach ($months as $x) {
+        if ($x === $month) {
+            $body .= "<option value='$x' selected='selected'>$x";
+        } else {
+            $body .= "<option value='$x'>$x";
+        }
+    }
+                
+    $body .= "</select>
+                <select name='year'>";
                 
                 for($i=2012; $i<=date("Y"); $i++){
-                    $body.='<option value='.$i.'>'.$i;
+                    $body.='<option value='.$i;
+                    
+                    if ($i == $year) { //Keeps current year selected
+                        $body .= " selected='selected'>";
+                    } else {
+                        $body .= ">";
+                    }
+                    $body .= $i;
                 }
                 
                 $week = array('Sunday'=>0, 'Monday'=>1, 'Tuesday'=>2, 'Wednesday'=>3, 'Thursday'=>4, 'Friday'=>5, 'Saturday'=>6);
@@ -136,6 +140,5 @@ HEREDOC;
             }
         </script>
 HEREDOC;
-
-    echo generatePage($body);
+    echo generatePage($body, "Calendapp");
 ?>
