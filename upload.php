f6731704424368147151838002bdeeabfee16529
Gabby Larios <lariosa1@yahoo.com>
2015-07-07 17:39:42 -0400

    Upload CSS done
    I didn't change anything, the only thing I did was add ids, images, and
    text

--- upload.php
+++ upload.php
@@ -6,16 +6,30 @@
 	session_start();
 	
     $body =<<<BODY
-     <form action="upload.php" enctype="multipart/form-data" method="post">
-      <p>
-   	     <b>Enter image to upload: </b>
-   	     <input type="file" name="filename" required="required" /><br />
-		 <strong>Caption: </strong><input type="text" required="required" name="caption" /><br />
-		 <strong>Tags (comma separated): </strong><input type="text" id='tags' name="tags" required="required" /><br />
-   	     <input type="submit" name='submit' value="Post" />
-      </p>
-      
-      </form>
+	 <html>
+	   <head>
+	     <title>Upload</title>
+		 <link rel='stylesheet' href='upload.css' type='text/css' />
+	   </head>
+       <body>
+	        <a href="main.php"> <img src="img/CalendAppLogo.png" width="256" height="73" alt="CalendApp" id="logo"> </a>
+	        <div id='subtitle'>
+                <img src='img/upload.png' alt='Upload' id='subtitle'>
+            </div>
+			<form action="upload.php" enctype="multipart/form-data" method="post" id='form'>
+			<h3> Select an image that you would lke to upload. Include a caption and at least two tags</h3>
+				 <p>
+					Enter image to upload: &nbsp;
+					<input type="file" name="filename" required="required" /><br />
+					Caption: &nbsp; <input type="text" required="required" name="caption" /><br />
+					Tags (comma separated): &nbsp; <input type="text" id='tags' name="tags" required="required" /><br />
+					<br>
+					<input type="submit" name='submit' value="Post" id='post'/>
+				</p>   
+		   </form>
+	   </body>
+	  </html>
+
 BODY;
 
     echo generatePage($body);
